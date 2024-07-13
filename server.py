72c328956cc9e24381724341c5d164d569efba2e
Андрій <101985948+andrijbikov134@users.noreply.github.com>
2024-07-13 12:52:15 +0300

    Update server.py

--- server.py
+++ server.py
@@ -52,11 +52,3 @@ def index():
 
 if __name__ == '__main__':
         app.run(debug=True, host='0.0.0.0',  port=5000)
-
-
-@app.route('/')
-def index():
-	return render_template('index.html')
-
-if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0',  port=5000)
