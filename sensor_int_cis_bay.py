46d7507e3ffee3cb5a6e4dd34f0a6652302ec45b
Vladyslav Shavarskyi <shavawork@gmail.com>
2024-07-13 10:13:44 +0000

    Update sensor_int_cis_bay.py to include IBM, Bayractar, and Cisco

--- sensor_int_cis_bay.py
+++ sensor_int_cis_bay.py
@@ -2,7 +2,7 @@ import time
 import random
 import requests
 
-companies = ["Intel", "Bayractar", "Cisco"]
+companies = ["IBM", "Bayractar", "Cisco"]
 
 def generate_price():
     return round(random.uniform(100, 500), 2)
@@ -10,7 +10,6 @@ def generate_price():
 while True:
     data = {company: generate_price() for company in companies}
     try:
-        # Зміна URL-адреси на localhost для роботи на будь-якій IP
         response = requests.post('http://localhost:5000/update', json=data)
         print("Data sent:", data)
         print("Server response:", response.json())
